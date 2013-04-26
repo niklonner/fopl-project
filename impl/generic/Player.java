6303784cb91fc7764c45aa62221fb13311d6d8d9
Niklas Lönnerfors <niklas_lonnerfors@yahoo.se>
2013-04-26 12:17:06 +0200

    Continuing work on tournament datastructure(s)

--- impl/generic/Player.java
+++ impl/generic/Player.java
@@ -1,27 +1,64 @@
 package generic;
 
-public class Player {
-    private String name;
-    private int result;
-    private boolean resultSet;
+import java.util.*;
 
+public class Player<ResultType extends Comparable<? super ResultType>>
+    implements Comparable<Player<ResultType>> {
+    private String name;
+    private Map<String, Object> attributes = new HashMap<>();
+    private static int nextId = 0;
+
+    public Player() {
+	this("player " + (nextId++));
+    }
+    
     public Player(String name) {
 	this.name = name;
+	attributes.put("result", null);
     }
     
     public String getName() {
 	return name;
     }
 
-    public void setResult(int result) {
-	this.result = result;
+    @SuppressWarnings("unchecked")
+    public ResultType setResult(ResultType result) {
+	return (ResultType) attributes.put("result", result);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ResultType getResult() {
+	Object result = attributes.get("result");
+	if (result == null) {
+	    throw new IllegalStateException("No result is set for player " + name);
+	} else {
+	    return (ResultType) result;
+	}
+    }
+
+    public void resetResult() {
+	attributes.remove("result");
     }
 
     public boolean resultIsSet() {
-	return resultSet;
+     	return attributeIsSet("result");
     }
 
-    public int getResult() {
-	return result;
+    public Object getAttribute(String key) {
+	return attributes.get(key);
+    }
+
+    public Object set(String key, Object value) {
+	return attributes.put(key, value);
+    }
+
+    public boolean attributeIsSet(String key) {
+	return attributes.containsKey(key);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // Default ordering is highest score first.
+    public int compareTo(Player<ResultType> other) {
+	return ((ResultType) attributes.get("result")).compareTo((ResultType) other.attributes.get("result"));
     }
 }
