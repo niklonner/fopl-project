fd6da160802109d5dd205d721748d1620cfe2610
Daniel Ström <D@nielstrom.se>
2013-04-28 15:53:28 +0200

    Correct the makefile to the new bnfc-structure and complete the PlayerParser.

--- bnfc/Play/Play/PlayerParser.java
+++ bnfc/Play/Play/PlayerParser.java
@@ -2,6 +2,7 @@ package Play;
 
 import Play.*;
 import Play.Absyn.*;
+import generic.Player;
 
 import java_cup.runtime.*;
 import java.io.*;
@@ -9,17 +10,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class PlayerParser {
-    List<Player> players;
-
-    private class Player {
-        //TODO: Change for the real player class when refactoring the package structure
-    }
-
-
     /**
      * For command-line testing
      */
     public static void main(String args[]) throws Exception {
+        List<Player> players = new ArrayList<>();
         Yylex l = null;
         parser p;
         try {
@@ -47,12 +42,14 @@ public class PlayerParser {
             System.out.println();
             System.out.println(PrettyPrinter.print(parse_tree));
 
+            System.out.println();
             System.out.println("[Parsing output]");
+            System.out.println();
 
             Parser parser = new Parser();
             parse_tree.accept(parser);
-        }
-        catch(Throwable e) {
+            players = parser.getPlayers();
+        } catch(Throwable e) {
             System.err.println("At line " + String.valueOf(l.line_num()) + ", near \"" + l.buff() + "\" :");
             System.err.println("     " + e.getMessage());
             System.exit(1);
@@ -60,7 +57,7 @@ public class PlayerParser {
     }
 
     public List<Player> parse(String path) {
-        players = new ArrayList<>();
+        List<Player> players = new ArrayList<>();
         Yylex l = null;
         parser p;
 
@@ -79,20 +76,32 @@ public class PlayerParser {
 
             Parser parser = new Parser();
             parse_tree.accept(parser);
+            players = parser.getPlayers();
         } catch(Throwable e) {
             System.err.println("At line " + String.valueOf(l.line_num()) + ", near \"" + l.buff() + "\" :");
             System.err.println("     " + e.getMessage());
             System.exit(1);
         }
 
+        System.out.println("Number of player objects: " + players.size());
+
         return players;
     }
 
     private static class Parser implements Visitor {
+        List<Player> players = new ArrayList<>();
+        Player<Integer> currentPlayer;
+        String currentAttribute;
+
+        public List<Player> getPlayers() {
+            return players;
+        }
+
         public void visitProg(Play.Absyn.Prog prog) {} //abstract class
         public void visitProgram(Play.Absyn.Program program) {
             /* Code For Program Goes Here */
-            System.out.println("lskjdflksj");
+
+            System.out.println("Visiting Program");
 
             if (program.listplayer_ != null) {program.listplayer_.accept(this);}
         }
@@ -106,9 +115,13 @@ public class PlayerParser {
         }
         public void visitPlayer(Play.Absyn.Player player) {
             /* Code For Player Goes Here */
+            System.out.println("Visiting Player");
+            System.out.println(player.ident_);
 
-            visitIdent(player.ident_);
+            currentPlayer = new Player<Integer>(player.ident_);
+            System.out.println("==============");
             if (player.listattr_ != null) {player.listattr_.accept(this);}
+            players.add(currentPlayer);
         }
         public void visitListAttr(Play.Absyn.ListAttr listattr) {
             while(listattr!= null)
@@ -122,28 +135,36 @@ public class PlayerParser {
         public void visitAttribute(Play.Absyn.Attribute attribute) {
             /* Code For Attribute Goes Here */
 
-            visitIdent(attribute.ident_);
+            System.out.println("Visiting Attribute");
+            currentPlayer.set(attribute.ident_, true);
         }
         public void visitValueAttribute(Play.Absyn.ValueAttribute valueattribute) {
             /* Code For ValueAttribute Goes Here */
+            System.out.println("Visiting Value Attribute");
+            currentAttribute = valueattribute.ident_;
 
-            visitIdent(valueattribute.ident_);
             valueattribute.val_.accept(this);
         }
         public void visitVal(Play.Absyn.Val val) {} //abstract class
         public void visitIntegerValue(Play.Absyn.IntegerValue integervalue) {
             /* Code For IntegerValue Goes Here */
 
+            System.out.println("Visiting Integer Value");
+            currentPlayer.set(currentAttribute, integervalue.integer_);
             visitInteger(integervalue.integer_);
         }
         public void visitDoubleValue(Play.Absyn.DoubleValue doublevalue) {
             /* Code For DoubleValue Goes Here */
 
+            System.out.println("Visiting Double Value");
+            currentPlayer.set(currentAttribute, doublevalue.double_);
             visitDouble(doublevalue.double_);
         }
         public void visitStringValue(Play.Absyn.StringValue stringvalue) {
             /* Code For StringValue Goes Here */
 
+            System.out.println("Visiting String value");
+            currentPlayer.set(currentAttribute, stringvalue.string_);
             visitString(stringvalue.string_);
         }
         public void visitIdent(String i) {}
