3cc08e28e8f1236a726300422895ffd147fe6b59
Niklas Lönnerfors <niklas_lonnerfors@yahoo.se>
2013-05-07 10:58:48 +0200

    Testing Brackets, no run done yet

--- impl/TestMain.java
+++ impl/TestMain.java
@@ -2,6 +2,8 @@ import java_cup.runtime.*;
 import Swag.*;
 import Swag.Absyn.*;
 import java.io.*;
+import java.util.*;
+import generic.*;
 
 public class TestMain {
 
@@ -9,5 +11,20 @@ public class TestMain {
     Swag.Absyn.Prog p = BasicParser.parseTournamentFile(args[0]);
     System.out.println("Alright, managed to parse!");
     //p.accept(new generic.GenericVisitor());
+    Player<Integer> p1 = new Player<>("Niklas");
+    Player<Integer> p2 = new Player<>("Daniel");
+    Player<Integer> p3 = new Player<>("Daniel");
+    Player<Integer> p4 = new Player<>("Daniel");
+    Player<Integer> p5 = new Player<>("Daniel");
+    Player<Integer> p6 = new Player<>("Daniel");
+    Player<Integer> p7 = new Player<>("Daniel");
+    Player<Integer> p8 = new Player<>("Daniel");
+    List<Player<Integer>> players = Arrays.asList(p1,p2,p3,p4,p5,p6,p7,p8);// new Play.PlayerParser().parse();
+    Bracket.Builder<Integer> b = new Bracket.Builder<>();
+    b.groupby(4).playuntil(2).advance(1).setPlayers(players);
+    Bracket<Integer> br = new Bracket<>(b);
+    br.startBuild();
   }
+
+    private class NodeObserver{}
 }
