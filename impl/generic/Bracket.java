28c94097ac93744c23890f37d70613032f60649c
Niklas Lönnerfors <niklas_lonnerfors@yahoo.se>
2013-05-09 14:14:35 +0200

    entering players to arbitrary levels should now work

--- impl/generic/Bracket.java
+++ impl/generic/Bracket.java
@@ -12,6 +12,8 @@ import util.*;
 // sendtopernode
 
 public class Bracket<ResultType extends Comparable<? super ResultType>> extends SubTournament<ResultType> {
+    private static Random rnd = new Random();
+    
     private int groupBy;
     private int advancing;
     private int playUntil;
@@ -64,6 +66,11 @@ public class Bracket<ResultType extends Comparable<? super ResultType>> extends 
             return this;
         }
 
+        public Builder<ResultType> groupBy(double groupBy) {
+            subTournament.groupBy = (int)groupBy;
+            return this;
+        }
+        
         public Builder<ResultType> playUntil(int playUntil) {
             subTournament.playUntil = playUntil;
             return this;
@@ -77,7 +84,7 @@ public class Bracket<ResultType extends Comparable<? super ResultType>> extends 
         public Builder<ResultType> sendLosersTo(PlayerReceiver<ResultType> receiver, SetModifier<Player<ResultType>> mod) {
             return sendToPerNode(receiver,new BottomMod<Player<ResultType>>(subTournament.groupBy-subTournament.advancing, mod));
         }
-
+        
         public Builder<ResultType> sendToPerNode(PlayerReceiver<ResultType> receiver, SetModifier<Player<ResultType>> mod) {
             subTournament.perNodeReceivers.add(new Pair<>(receiver, mod));
             return this;
@@ -119,12 +126,60 @@ public class Bracket<ResultType extends Comparable<? super ResultType>> extends 
         previousLayer.connectWith(finalLayer);
         built = true;
         System.out.println(nodes.size() + " nodes");
+        dummyRun();
+    }
+
+    protected void dummyRun() {
+        // add players
+        int level = 0;
+        BracketNodeLayer prevLayer = null;
+        for (BracketNodeLayer bnl : nodeLayers) {
+            Iterator<BracketNode<ResultType>> it = bnl.nodes.iterator();
+            BracketNode<ResultType> node = null;
+            //            while (it.hasNext() && (node = it.next()).isFull()) {
+            //            }
+            // advance past occupied nodes
+            int playerCounter = prevLayer == null ? 0 :
+                prevLayer.nodes.size()*advancing;
+            int occupiedNodes = prevLayer == null ? 0 :
+                (int)Math.ceil((double)playerCounter/groupBy);
+            System.out.println("occupiedNodes " + occupiedNodes);
+            System.out.println(playerCounter);
+            for (int i=0;i<occupiedNodes;i++) {
+                node = it.next();
+                System.out.println(node.getId());
+            }
+            for (Player<ResultType> p : players) {
+                if ((!p.attributeIsSet("level") && level==0) || (p.attributeIsSet("level") && (int)p.get("level") == level)) {
+                    System.out.println("add player " + p.getId());
+                    if (playerCounter % groupBy == 0) {
+                        node = it.next();
+                    }
+                    node.acceptPlayer(p);
+                    playerCounter++;
+                }
+            }
+            level++;
+            prevLayer = bnl;
+        }
+        // now randomize results
+        // hard-coded to use Integer result type
+        for (BracketNodeLayer bnl : nodeLayers) {
+            for (BracketNode<ResultType> n : bnl.nodes) {
+                for (Player<ResultType> p : n.getPlayers()) {
+                    //                    System.out.println(n.getId());
+                    //                    System.out.println(n.getPlayers());
+                    ((BracketNode<Integer>)n).addResult(p.getId(),rnd.nextInt(100));
+                }
+            }
+        }
+
     }
 
     private int numPlayersWithLevel(int level) {
         int ret = 0;
         for (Player p : players) {
-            if ((!p.attributeIsSet("level") && level==0) || (p.attributeIsSet("level") && (int)p.getAttribute("level")==level)) {
+            if ((!p.attributeIsSet("level") && level==0) || (p.attributeIsSet("level") && (int)p.get("level")==level)) {
                 ret++;
             }
         }
@@ -158,7 +213,6 @@ public class Bracket<ResultType extends Comparable<? super ResultType>> extends 
                 builder.setNumPlayers(groupBy);
                 builder.setObservers(observers);
                 // set comparator
-                // set observers
                 BracketNode<ResultType> newNode = new BracketNode<ResultType>(builder);
                 nodes.add(newNode);
                 Bracket.this.nodes.add(newNode);
@@ -171,9 +225,9 @@ public class Bracket<ResultType extends Comparable<? super ResultType>> extends 
 
         void connectWith(BracketNodeLayer next) {
             int treeWidth = groupBy/advancing; // number of nodes in previous level to connect to each node in new level
-            for (int i=0;i<=nodes.size()-treeWidth;i+=treeWidth) {
+            for (int i=0;i<nodes.size();i+=treeWidth) {
                 BracketNode<ResultType> nextNode = next.nodes.get(i/treeWidth);
-                for (int j=0;j<treeWidth;j++) {
+                for (int j=0;j<treeWidth && i+j<nodes.size();j++) {
                     BracketNode<ResultType> n1 = nodes.get(i+j);
                     n1.addReceiver(nextNode,advancingMod);
                     for (Pair<PlayerReceiver<ResultType>, SetModifier<Player<ResultType>>> p : perNodeReceivers) {
